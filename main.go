749023c99d7d0c7fffbee2cd1780b3e19070316c
SautKoinworks <saut.sihotang@koinworks.com>
2022-08-27 18:36:37 +0700

    menampilkan  Error validation

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
@@ -53,8 +56,8 @@ func queryHandler(c *gin.Context) {
 }
 
 type BookInput struct {
-	Title string `json:"title" binding:"required"`
-	Price int    `json:"price" binding:"required,number"`
+	Title string      `json:"title" binding:"required"`
+	Price json.Number `json:"price" binding:"required,number"`
 }
 
 func postBookHandler(c *gin.Context) {
@@ -63,10 +66,13 @@ func postBookHandler(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&bookInput)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"msg": "data salah",
-		})
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMassage := fmt.Sprintf("Error on filed %s, condition: %s", e.Field(), e.ActualTag())
+			c.JSON(http.StatusBadRequest, errorMassage)
+
+		}
 		return
+
 	}
 
 	c.JSON(http.StatusOK, gin.H{
