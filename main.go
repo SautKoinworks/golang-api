d960d7cb8a114fa9293d6b6cd3ededb2302fbf11
SautKoinworks <saut.sihotang@koinworks.com>
2022-08-29 10:44:40 +0700

    database connection

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
 
 	"pustaka-api/handler"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	dsn := "host=localhost user=postgres password=postgres dbname=pustaka-api port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("DB connection Error")
+	}
+
+	fmt.Println("Database connection succeed")
 	router := gin.Default()
 
 	v1 := router.Group("/v1")
